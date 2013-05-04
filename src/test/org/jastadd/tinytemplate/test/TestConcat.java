9590b6c4925bb57d3f77d2a87d76efac52cf2cdd
Niklas Fors <niklas.fors@cs.lth.se>
2013-05-04 12:53:11 +0200

    Updated copyright text

--- src/test/org/jastadd/tinytemplate/test/TestConcat.java
+++ src/test/org/jastadd/tinytemplate/test/TestConcat.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, Jesper Öqvist <jesper@cs.lth.se>
+/* Copyright (c) 2013, Niklas Fors <niklas.fors@cs.lth.se>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
